b9437195a75c3ff8a9642ab6917bb60c898993f1
S <s>
2012-04-16 11:07:05 -0400

    {"type":"TI", "runs":"0"}

--- Hangman.java
+++ Hangman.java
@@ -22,7 +22,7 @@ public class Hangman extends ConsoleProgram {
     	lexiconObject = new HangmanLexicon();
     	int wordIndex = rgen.nextInt(0,lexiconObject.getWordCount());
     	word = lexiconObject.getWord(wordIndex);
-    	lettersremaining = word.length();
+    	lettersRemaining = word.length();
     }
     private void beginGame() {
     	println("Welcome to Hangman!");
