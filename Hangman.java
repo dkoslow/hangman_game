e28d06e8adfee339e1fe7ba220cedb7003f10a90
S <s>
2012-04-16 10:55:31 -0400

    {"type":"TI", "runs":"0"}

--- Hangman.java
+++ Hangman.java
@@ -44,7 +44,7 @@ public class Hangman extends ConsoleProgram {
     	while(true) {
     		int pos = word.indexOf(checkedGuess);
     		if (pos >= 0) {
-    			wordMarked = wordMarked.substring(0, pos) + checkedGuess + wordMarked.substring(pos + 1)
+    			wordMarked = wordMarked.substring(0, pos) + checkedGuess + wordMarked.substring(pos + 1);
     		} else
     			break;
     	}
