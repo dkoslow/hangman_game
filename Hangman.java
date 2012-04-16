7002339f618efc77f1939965c157ef00079792a2
S <s>
2012-04-16 10:32:28 -0400

    {"type":"TI", "runs":"0"}

--- Hangman.java
+++ Hangman.java
@@ -28,6 +28,7 @@ public class Hangman extends ConsoleProgram {
     	if( guessesRemaining > 0 ) {
     		if( lettersRemaining > 0 ) {
     			println("The word now looks like this: " + wordMarked);
+    			println("You have " + guessesRemaining + " left.")
     			
     		}
     	}
