d8293dda352b8d1e0dc5daace75bcf8b42561890
S <s>
2012-04-16 15:30:31 -0400

    {"type":"TI", "runs":"0"}

--- Hangman.java
+++ Hangman.java
@@ -71,8 +71,6 @@ public class Hangman extends ConsoleProgram {
     	}
     	if (y > 0) {
     		lettersRemaining = lettersRemaining - y;
-    		canvas.removeWord();
-    		canvas.displayWord();
     		if (y != 1) {
     			println("Correct: There are " + y + " " + checkedGuess + "'s in the word.");
     		} else {
