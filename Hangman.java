e957b328360e299dfb269e2616809db7e2a832db
S <s>
2012-04-16 11:05:24 -0400

    {"type":"TI", "runs":"0"}

--- Hangman.java
+++ Hangman.java
@@ -29,7 +29,6 @@ public class Hangman extends ConsoleProgram {
     		wordMarked = wordMarked + "-";
     	}
     	while ( guessesRemaining > 0 ) {
-    		checkLettersRemaining();
     		while ( lettersRemaining > 0 ) {
     			println("The word now looks like this: " + wordMarked);
     			println("You have " + guessesRemaining + " left.");
