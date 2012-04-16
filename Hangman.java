fce055d1b0d496b66169c9e43c5ac3e26e0ff27c
S <s>
2012-04-16 12:07:18 -0400

    {"type":"TI", "runs":"0"}

--- Hangman.java
+++ Hangman.java
@@ -92,5 +92,5 @@ public class Hangman extends ConsoleProgram {
     private int guessesRemaining = 8;
     private int lettersRemaining;
     private String wordMarked;
-    private Boolean lastGuess;
+    private Boolean isGuessCorrect;
 }
