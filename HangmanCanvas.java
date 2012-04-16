af927e82452384214cff353e47a27c26c0abfe51
S <s>
2012-04-16 15:10:46 -0400

    {"type":"TI", "runs":"0"}

--- HangmanCanvas.java
+++ HangmanCanvas.java
@@ -28,7 +28,7 @@ public class HangmanCanvas extends GCanvas {
 		String showWord = wordForDisplay.getWordMarked();
 		GLabel wordDisplay = new GLabel("test", 50, 50);
 		wordDisplay.setFont("Times-42");
-		wordDisplay.setColor(COLORS.red);
+		wordDisplay.setColor(Color.Red);
 		add(wordDisplay, 50, 50);
 	}
 
