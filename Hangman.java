6ee9a51e3d2f9c79d295d53aac0cd8f3d057fdce
S <s>
2012-04-16 11:44:56 -0400

    {"type":"TI", "runs":"0"}

--- Hangman.java
+++ Hangman.java
@@ -37,7 +37,7 @@ public class Hangman extends ConsoleProgram {
     			println("You have " + guessesRemaining + " left.");
     			String guess = readLine("Your guess: ");
     			if ( guess.length() == 1 ) {
-    				guess = guess.Upcase();
+    				guess = guess.upcase();
     				wordMarked = checkGuess(guess);
     				if (lastGuess == true) {
     					correctGuess();
