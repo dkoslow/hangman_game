f6910ce0dc632b0deae01a15e4e8f9dc269f15ae
S <s>
2012-04-16 11:59:06 -0400

    {"type":"TI", "runs":"0"}

--- Hangman.java
+++ Hangman.java
@@ -54,7 +54,7 @@ public class Hangman extends ConsoleProgram {
     }
     private String checkGuess(char checkedGuess) {
     	int i = 0;
-    	for ( i = 0; i < word.length; i++) {
+    	for ( i = 0; i < word.length(); i++) {
     		if(word.charAt(i) == checkedGuess) {
     			
     		}
