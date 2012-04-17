288d4f20bcbf860f80bcf0d0e89e7ea63a3f84d4
S <s>
2012-04-17 17:18:14 -0400

    {"type":"TI", "runs":"0"}

--- HangmanCanvas.java
+++ HangmanCanvas.java
@@ -82,6 +82,9 @@ public class HangmanCanvas extends GCanvas {
 		add(shoulderR);
 		add(armR);
 	}
+	private void addLegL() {50 + BEAM_LENGTH, 50 + ROPE_LENGTH + 2 * HEAD_RADIUS + BODY_LENGTH, 50 + BEAM_LENGTH - HIP_WIDTH, 50 + ROPE_LENGTH + 2 * HEAD_RADIUS + BODY_LENGTH
+		
+	}
 	private GLabel wordDisplay;
 	private GLabel incorrectGuessLabel;
 
