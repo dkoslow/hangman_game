4646799535b9b66f12b2ed0c9c2b74a4f81db17e
S <s>
2012-04-17 12:43:15 -0400

    {"type":"TI", "runs":"0"}

--- Hangman.java
+++ Hangman.java
@@ -81,7 +81,7 @@ public class Hangman extends ConsoleProgram {
     	}
     	return wordMarked;
     }
-    private String getWord {
+    private String getWord() {
     	return wordMarked;
     }
     
