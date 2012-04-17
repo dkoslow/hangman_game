ec4e0fb2eba47aeb7614e1d8a5e37e32b310961b
S <s>
2012-04-17 12:51:37 -0400

    {"type":"TI", "runs":"1"}

--- HangmanCanvas.java
+++ HangmanCanvas.java
@@ -28,7 +28,7 @@ public class HangmanCanvas extends GCanvas {
 	public void displayWord() {
 		Hangman holder = new Hangman();
 		String wordMarked = holder.getWord();
-		GLabel test = new GLabel ("test", 300, 50);
+		GLabel test = new GLabel ("test", 250, 50);
 		add(test);
 	}
 	public void removeWord() {
